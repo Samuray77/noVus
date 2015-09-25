ea3ca661d8db12fff6941b6310f983a3ad509df5
Pavel Svyatnenko <paha78sv@gmail.com>
2015-09-25 17:59:27 +0300

    AutomationTests noVus_Win commit 25.09.2015-2

--- src/main/java/Pages/LMSLoginPage.java
+++ src/main/java/Pages/LMSLoginPage.java
@@ -1,24 +1,33 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+import static com.codeborne.selenide.Selenide.$;
 
 public class LMSLoginPage {
 
-        private WebDriver driver;
+//        private WebDriver driver;
+//
+//        @FindBy(xpath = ".//*[@id='InputLogin']")
+//        WebElement loginField;
+//        @FindBy(xpath = ".//*[@id='InputPassword']")
+//        WebElement passwordField;
+//        @FindBy(xpath = "html/body/div[1]/div/div[2]/form/input[3]")
+//        WebElement buttonEnter;
+//        @FindBy(xpath = "html/body/div[1]/div/div[2]/p")
+//        WebElement checkText;
 
-        @FindBy(xpath = ".//*[@id='InputLogin']")
-        WebElement loginField;
-        @FindBy(xpath = ".//*[@id='InputPassword']")
-        WebElement passwordField;
-        @FindBy(xpath = "html/body/div[1]/div/div[2]/form/input[3]")
-        WebElement buttonEnter;
-        @FindBy(xpath = "html/body/div[1]/div/div[2]/p")
-        WebElement checkText;
+        //    Locators for registration
+        WebElement logo = $(By.xpath("html/body/div[1]/div/img"));
+        WebElement loginField = $(By.xpath(".//*[@id='id_username']"));
+        WebElement passwordField = $(By.xpath(".//*[@id='id_password']"));
+        WebElement inputLogin = $(By.xpath(".//*[@id='login-form']/div[4]/input"));
+
+        public void userCanRegistration(){
 
 
 
-
+        }
 }
--- src/test/java/UITest/UaLmsLoginPage.java
+++ src/test/java/UITest/UaLmsLoginPage.java
@@ -1,7 +1,31 @@
 package UITest;
 
-/**
- * Created by NumberOk on 25.09.2015.
- */
+
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class UaLmsLoginPage {
+
+    //    Locators for registration
+    WebElement loginField = $(By.xpath(".//*[@id='id_username']"));
+    WebElement passwordField = $(By.xpath(".//*[@id='id_password']"));
+    WebElement inputLogin = $(By.xpath(".//*[@id='login-form']/div[4]/input"));
+
+    @Before
+    public void loginPageAuthorizationTest() throws Exception {
+
+//        Open AdminPage
+        open("http://127.0.1.1/admin/");
+        //        Authorization AdminPage
+        $(loginField).setValue("lms");
+        $(passwordField).setValue("lms");
+        $(inputLogin).click();
+
+        System.out.println("Authorization AdminPage - PASSED");
+    }
+
 }
